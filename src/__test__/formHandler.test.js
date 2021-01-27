2a190484ba74562a8292b7a80387532742cc5434
Sanrambo <gh.r4mbo@gmail.com>
2021-01-27 14:45:00 +0300

    Commit

--- src/__test__/formHandler.test.js
+++ src/__test__/formHandler.test.js
@@ -2,19 +2,19 @@ import { handleSubmit, postData, updateUI } from '../client/js/formHandler'
 
 
 describe("Testing the handleSubmit() functionality", () => {
-    test("It should be functionality ", async () => {
+    test("Testing the handleSubmit() functionality", async () => {
         expect(handleSubmit).toBeInstanceOf(Function);
     });
 
 });
 
-describe("Testing the meaningcloud() functionality", () => {
-    test("It should be functionality ", async () => {
+describe("Testing the postData() functionality", () => {
+    test("Testing the postData() functionality", async () => {
         expect(postData).toBeInstanceOf(Function);
     });
 });
-describe("Testing the meaningcloud() functionality", () => {
-    test("It should be functionality ", async () => {
+describe("Testing the updateUI() functionality", () => {
+    test("Testing the UpdateUI() functionality", async () => {
         expect(updateUI).toBeInstanceOf(Function);
     });
 });
